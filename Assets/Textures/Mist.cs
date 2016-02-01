db25c980caf5b5eda146dfdf3d7fb79029ac2564
Christian Kokott <christian.kokott@gmail.com>
2016-02-01 20:06:11 +0100

    added mist menu

---
+++ Assets/Textures/Mist.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mist : MonoBehaviour {
+
+    public float Speed = 10;
+
+	void Start () {
+
+	}
+
+	void Update () {
+        this.GetComponent<SpriteRenderer>().material.mainTextureOffset += Vector2.right * Speed * Time.deltaTime;
+	}
+}
